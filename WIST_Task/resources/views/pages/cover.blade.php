8573608a7069b3d1a5668c5c9857eefb5a5ee099
EnasHameed <alqaadienas@gmail.com>
2024-03-04 11:20:59 +0300

    marge website with laravel

---
+++ WIST_Task/resources/views/pages/cover.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <!-- fontawesome.com-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Main Template CSS File -->
+    <link rel="stylesheet" href="../../css/style.css" />
+
+    <title>Cover</title>
+  </head>
+  <body>
+
+<div class="grid-container">
+  <div class="grid-item">  <div class="div2"><img src="../assets/img/div2.png" alt="" /></div></div>
+  <div class="grid-item"></div>
+  <div class="grid-item">  <div class="div1"><img src="../assets/img/div1.png" alt="" /></div></div>
+  <div class="grid-item"><div class="div4"><img src="../assets/img/div4.png" alt="" /></div></div>
+  <div class="grid-item center">
+
+        <p class="fivehanderd">500</p>
+        <p class="websection">
+          Website Sections
+        </p>
+       <a href="info.html">
+        <div class="cover">
+        <button >
+          PRO Pack
+        </button></a>
+        </div>
+  </div>
+
+  <div class="grid-item"><div class="div3"><img src="../assets/img/div3.png" alt="" /></div></div>
+  <div class="grid-item"><div class="div6"><img src="../assets/img/div6.png" alt="" /></div></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"><div class="div5"><img src="../assets/img/div5.png" alt="" /></div></div>
+</div>
+
+
+
+
+  </body>
+</html>
---
+++ WIST_Task/resources/views/pages/info.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <!-- Main Template CSS File -->
+    <link rel="stylesheet" href="../../../public/assets/css/style.css" />
+    <title>Info</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="info">
+        <div class="want_more">
+          <p>Want more?</p>
+          <p>1000 Website Sections - FULL Pack +</p>
+        </div>
+
+        <div class="free">
+          <p>+ Free Design Systems</p>
+          <p>+ Free UI Kits</p>
+          <p>+ Free UI Chart Libraries</p>
+
+          <a href="https://danitesler.com/my-templates/">
+            https://danitesler.com/my-templates/</a
+          >
+        </div>
+<div class="line">
+
+</div>
+        <div class="want_more">
+          <p>Share your feedback:</p>
+          <a href="https://twitter.com/danitesler">
+            https://twitter.com/danitesler</a>
+<br>
+         <a href="cover.html"> cover page</a>
+       <br>
+         <a href="../index.html">Acmy</a>
+        </div>
+
+      </div>
+
+    </div>
+  </body>
+</html>
